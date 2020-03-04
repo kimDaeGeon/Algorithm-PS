6158e9061683a609408e78af6b7790339b0f3f1c
kimDaeGeon <52105661+kimDaeGeon@users.noreply.github.com>
2020-03-04 22:37:47 +0900

    20200304

--- march 2020/march 2020/10844.cpp
+++ march 2020/march 2020/10844.cpp
@@ -1,26 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int dp[101][10] = { {}, {1,1,1,1,1,1,1,1,1,1} };
+long long dp[101][10];
 
 int main() {
-	int n;
-	cin >> n;
+    int n;
+    cin >> n;
+    for (int i = 1; i <= 9; i++) {
+        dp[1][i] = 1;
+    }
+    for (int i = 2; i <= n; i++) {
+        for (int j = 0; j <= 9; j++) {
+            dp[i][j] = 0;
+            if (j - 1 >= 0) {
+                dp[i][j] += dp[i - 1][j - 1];
+            }
+            if (j + 1 <= 9) {
+                dp[i][j] += dp[i - 1][j + 1];
+            }
+            dp[i][j] %= 1000000000;
+        }
+    }
 
-	for (int i = 2; i <= 100; i++) {
-		dp[i][1] = dp[i - 1][1];
-		dp[i][9] = dp[i - 1][9];
-		for (int j = 2; j <= 8; j++) {
-			dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
-		}
-	}
-
-	int stair = 0;
-	for (int i = 1; i <= 9; i++) {
-		stair += dp[n][i];
-	}
-	cout << stair << '\n';
-
-
-	return 0;
+    long long sum = 0;
+    for (int i = 0; i <= 9; i++) {
+        sum += dp[n][i];
+    }
+    sum %= 1000000000;
+    cout << sum << '\n';
+    return 0;
 }
---
+++ march 2020/march 2020/14645.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cin >> a >> b;
+	}
+
+	cout << "ºñ¿ÍÀÌ" << '\n';
+
+	return 0;
+}
